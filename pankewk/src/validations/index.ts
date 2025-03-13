9da583f30a5582d67bb5ef302e61c49cd3fcc41a
ELEczek <szymon.dwax@gmail.com>
2025-03-13 23:13:22 +0100

    Delete pankewk directory

--- pankewk/src/validations/index.ts
+++
@@ -1,9 +0,0 @@
-import basic from './basic';
-import aaveSpaceValidation from './aave';
-import nounsSpaceValidation from './nouns';
-
-export default {
-  basic,
-  aave: aaveSpaceValidation,
-  nouns: nounsSpaceValidation
-};
